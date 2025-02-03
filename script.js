51fa828e3f5f5ee056dd2c5b22b17ea83d0ba580
Dylan Fitch <kkarl30@gmail.com>
2025-02-02 21:17:01 -0500

    finished insertAt, contains, and find

--- script.js
+++ script.js
@@ -27,6 +27,10 @@ class LinkedList {
         this.length = 0;
     }
 
+    get len(){
+        return this.length;   
+    }
+
     head(){
         return this.#head.next
     }
@@ -58,13 +62,99 @@ class LinkedList {
         this.length += 1;
     }
 
-    size(){
-        return this.length;   
+
+    at(index){
+
+        if (index >= this.length){
+            return null;
+        }
+
+        let cur = this.#head.next;
+        let i = 0;
+        while(i != index){
+            cur = cur.next;
+            i++;
+        }
+
+        return cur;
+
+    }
+
+  
+
+    pop(){
+
+        if(this.#head.next === null){
+            return null;
+        }
+
+        this.length--;
+
+        let cur = this.#head.next;
+        let prev = this.#head;
+        while(cur.next){
+            prev = cur;
+            cur = cur.next;
+        }
+
+        prev.next = null;
+        return cur;
+
+    }
+
+    contains(value){
+
+        let cur = this.#head.next;
+        while(cur){
+            if(cur.value === value){
+                return true;
+            }
+            cur = cur.next;
+        }
+
+        return false;
+    }
+
+    find(value){
+
+        let cur = this.#head.next;
+        let index = 0;
+        while(cur){
+            if(cur.value === value){
+                return index;
+            }
+            cur = cur.next;
+            index++;
+        }
+
+        return null;
+    }
+
+    insertAt(value, index){
+
+        if(index >= this.length){
+            return
+        }
+
+        this.length++;
+        const newNode = new Node(value);
+        let cur = this.#head.next;
+        let prev = this.#head;
+        let i = 0;
+        while (i != index){
+            prev = cur;
+            cur = cur.next;
+            i++;
+        }
+
+        prev.next = newNode;
+        newNode.next = cur;
+
     }
 
 
     toString(){
-        let str = '';
+        let str = ' ( HEAD ) -> ';
         let cur = this.#head.next;
         while(cur){
             str += `( ${cur.value} ) -> `;
@@ -86,8 +176,5 @@ list.append(new Node(12));
 list.append(new Node(1));
 list.prepend(new Node(-5));
 list.prepend(new Node(1233));
-console.log(list.size());
-console.log(list.toString());
 
-console.log(list.head().value);
-console.log(list.tail().value);
+
